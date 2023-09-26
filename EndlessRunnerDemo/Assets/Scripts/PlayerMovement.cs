17b708717bb39e734d96aff154acc96ff2f7b625
TunaYou <zshirikjian@gmail.com>
2023-09-26 18:04:24 -0400

    Began adding sliding

--- EndlessRunnerDemo/Assets/Scripts/PlayerMovement.cs
+++ EndlessRunnerDemo/Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     [Range(2,6)]
     public float holdButtonGravity = 2.5f;
 
+    public float slideGravity = 10f;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -25,29 +27,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && canJump)
+        //JUMPING//
+        if(Input.GetKeyDown(KeyCode.Space) && canJump && !Input.GetKey(KeyCode.LeftShift))
         {
             //ForceMode2D.Impulse --> immediately adds force to RB
             rb2d.velocity = Vector2.up * jumpSpeed;
             canJump = false;
         }
 
+        if(!Input.GetKey(KeyCode.LeftShift))
+        {
+            if(rb2d.velocity.y < 0)
+            {
+                    //Increase our gravity scale
+                rb2d.velocity += Vector2.up * Physics.gravity.y * (slideGravity - 1) * Time.deltaTime;
+            }
+
+            //If player lets go of space while still in the air, shorter jump
+            else if(rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+            {
+                    rb2d.velocity += Vector2.up * Physics.gravity.y * (tapButtonGravity - 1) * Time.deltaTime;
+            }
+
+            if(rb2d.velocity.y == 0)
+            {
+                canJump = true;
+            }
+        }
+        else if(Input.GetKey(KeyCode.LeftShift))
+        {
+            rb2d.velocity += Vector2.up * Physics.gravity.y * (slideGravity - 1) * Time.deltaTime;
+        }
+
                 //If player is falling (velocity is negative) and is still holding space
-        if(rb2d.velocity.y < 0)
-        {
-                //Increase our gravity scale
-            rb2d.velocity += Vector2.up * Physics.gravity.y * (holdButtonGravity - 1) * Time.deltaTime;
-        }
 
-        //If player lets go of space while still in the air, shorter jump
-        else if(rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-        {
-                rb2d.velocity += Vector2.up * Physics.gravity.y * (tapButtonGravity - 1) * Time.deltaTime;
-        }
 
-        if(rb2d.velocity.y == 0)
-        {
-            canJump = true;
-        }
+        //SLIDING
+       // if(Input.GetKeyDown(KeyCode.LeftShift))
+       // {
+       //     Debug.Log("Slide");
+        //    rb2d.velocity += Vector2.up * Physics.gravity.y * (slideGravity - 1) * Time.deltaTime;
+       // }
     }
 }
