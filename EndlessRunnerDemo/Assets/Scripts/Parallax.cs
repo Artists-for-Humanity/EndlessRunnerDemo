d7c16aab8019fdaef66ba6c8fd30a9b673e73540
TunaYou <zshirikjian@gmail.com>
2023-09-27 14:08:16 -0400

    BG now scrolls Automatically

--- EndlessRunnerDemo/Assets/Scripts/Parallax.cs
+++ EndlessRunnerDemo/Assets/Scripts/Parallax.cs
@@ -7,7 +7,7 @@ public class Parallax : MonoBehaviour
 
     private float length, startPosition;
     public GameObject camera;
-
+    public bool autoScroll;
     public float parallaxEffect;
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,29 @@ public class Parallax : MonoBehaviour
     {
         //Set the distance to be the current transform position 
         //Multiply it by the parallaxEffect (different for each layer)
+        float temp = (camera.transform.position.x * (1 - parallaxEffect));
         float dist = (camera.transform.position.x * parallaxEffect);
-        transform.position = new Vector2(startPosition + dist, transform.position.y);
-    }
+        
+        float desiredXPosition = startPosition + dist;
+
+        if(autoScroll)
+        {
+            //Pushes the BG to the left 
+            desiredXPosition = transform.position.x - parallaxEffect;
+        }
+
+        transform.position = new Vector2(desiredXPosition, transform.position.y);
+       //transform.position = new Vector2(startPosition + dist, transform.position.y);
+
+        //If the layer goes out of bounds, 
+        if(temp > startPosition + length)
+        {
+            startPosition += length;
+            Debug.Log("OUT OF BOUNDS");
+        }
+        else if(temp < startPosition - length) 
+        {
+            startPosition -= length;
+        }
+}
 }
