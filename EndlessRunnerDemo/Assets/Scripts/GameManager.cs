deb4c2569415185679c994917068f898fa3d1bda
TunaYou <zshirikjian@gmail.com>
2023-09-28 10:57:32 -0400

    Began Obstacle Spawning & GameOver UI

--- EndlessRunnerDemo/Assets/Scripts/GameManager.cs
+++ EndlessRunnerDemo/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
@@ -11,23 +12,36 @@ public class GameManager : MonoBehaviour
     public int playerHealth;
     public int score = 0;
     public bool invincible = false;
+    public bool gameOver = false;
+
     //REFERENCES//
     private PlayerMovement player;
 
     public TextMeshProUGUI gameOverText;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI finalScoreText;
+
+    public TextMeshProUGUI healhText;
+    //GameOver Panel for when players get Game Over
+    public GameObject restartPanel;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
         gameOverText.text = "";
+        score = 0;
+        restartPanel.SetActive(false);
+        healhText.text = playerHealth.ToString();
         scoreText.text = "0";
+        gameOver = false;
+        invincible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = score.ToString();
+        healhText.text = playerHealth.ToString();
     }
 
     public void TakeDamage()
@@ -41,6 +55,7 @@ public class GameManager : MonoBehaviour
             if(playerHealth <= 0)
             {
                 Debug.Log("Game Over");
+                gameOver = true;
                 GameOver();
             }
             invincible = true;
@@ -54,11 +69,22 @@ public class GameManager : MonoBehaviour
         Debug.Log("Game Over...");
         player.anim.SetTrigger("GameOver");
         gameOverText.text = "Game Over";
+        restartPanel.SetActive(true);
+        finalScoreText.text = score.ToString();
     }
 
     public void EndCoolDown()
     {
         invincible = false;
         player.anim.ResetTrigger("takingDamage");
+    }
+
+    //GAME OVER METHODS//
+
+    //Call this method when clicking the restart button.
+    //Reload the scene and try the game again.
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
---
+++ EndlessRunnerDemo/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    //OBSTACLE ARRAY OF THINGS TO SPAWN//
+public List<GameObject> obstacles = new List<GameObject>(); //Can be changed in the inspector
+public Vector2 spawnPosition; //Spawn Position of the ObstacleSpawner
+
+private float obstaclePosX;
+private float obstaclePosY;
+//REFERENCES//
+private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObstacles());
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnObstacles()
+    {
+        yield return new WaitForSeconds(1f);
+        while(gm.gameOver == false)
+        {
+            for(int i = 0; i < obstacles.Count; i++)
+            {
+                Debug.Log("Spawn an obstacle");
+                obstaclePosX = obstacles[i].transform.position.x + 20;
+                obstaclePosY = obstacles[i].transform.position.y;
+                spawnPosition = new Vector2(obstacles[i].transform.position.x + 20, obstacles[i].transform.position.y);
+                Instantiate(obstacles[i], spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(1f);
+            }
+            yield return new WaitForSeconds(3f);
+        }
+
+    }
+}
